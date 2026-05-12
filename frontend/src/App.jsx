be7e096e7ed73641622c7cbf0049febf3e44bd11
vedant-chautiwar <chautiwarvedant@gmail.com>
2026-05-12 21:40:52 +0530

    haha

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Navigate, Route, Routes, useLocation } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
@@ -10,21 +10,31 @@ function Private({ children }) {
   return localStorage.getItem("token") ? children : <Navigate to="/login" />;
 }
 
+function AppContent() {
+  const location = useLocation();
+  const isAuthPage = location.pathname === "/login" || location.pathname === "/signup";
+  const showNavbar = localStorage.getItem("token") && !isAuthPage;
+
+  return (
+    <div className="app-shell">
+      {showNavbar && <Navbar />}
+
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+
+        <Route path="/" element={<Private><Home /></Private>} />
+        <Route path="/discover" element={<Private><Discover /></Private>} />
+        <Route path="/profile" element={<Private><Profile /></Private>} />
+      </Routes>
+    </div>
+  );
+}
+
 export default function App() {
   return (
     <BrowserRouter>
-      <div className="app-shell">
-        {localStorage.getItem("token") && <Navbar />}
-
-        <Routes>
-          <Route path="/login" element={<Login />} />
-          <Route path="/signup" element={<Signup />} />
-
-          <Route path="/" element={<Private><Home /></Private>} />
-          <Route path="/discover" element={<Private><Discover /></Private>} />
-          <Route path="/profile" element={<Private><Profile /></Private>} />
-        </Routes>
-      </div>
+      <AppContent />
     </BrowserRouter>
   );
 }
